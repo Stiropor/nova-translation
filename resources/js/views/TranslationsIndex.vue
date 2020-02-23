4b0b2b643739a4424785f12a2e0b0e4560f73b16
Marko Bratkovič <marko.bratkovic@kreatorij.si>
2020-02-23 12:47:10 +0100

    Fix filter

--- resources/js/views/TranslationsIndex.vue
+++ resources/js/views/TranslationsIndex.vue
@@ -19,7 +19,7 @@
                 <router-link
                     class="cursor-pointer btn btn-default btn-primary"
                 :to="{ name: 'nova-translation.languages.translations.create', params: { language }}"
-                    
+
                     :title="__('Add language')"
                 >
                     {{ __('Add Translation') }}
@@ -30,13 +30,10 @@
         <loading-card :loading="loading" class="card">
 
             <div class="py-3 flex items-center border-b border-50">
-                
+
                 <div class="flex items-center ml-auto px-3">
                     <dropdown>
                         <dropdown-trigger
-                            slot-scope="{
-                                toggle,
-                            }"
                             :handle-click="toggle"
                             class="bg-30 px-3 border-2 border-30 rounded"
                             :class="{ 'bg-primary border-primary': filtersAreApplied }"
@@ -61,7 +58,7 @@
                                         @change="groupChanged"
                                     >
                                         <option value="">-----</option>
-                                        <option 
+                                        <option
                                             v-for="group in groups"
                                             :value="group"
                                             :key="group"
@@ -82,7 +79,7 @@
                                         :value="language"
                                         @change="languageChanged"
                                     >
-                                        <option 
+                                        <option
                                             v-for="language in languages"
                                             :value="language"
                                             :key="language"
@@ -116,9 +113,9 @@
                 </div>
             </div>
             <div v-if="!translations.length" class="flex justify-center items-center px-6 py-8">
-                
+
                 <div class="text-center">
-    
+
                     <svg
                         class="mb-3"
                         xmlns="http://www.w3.org/2000/svg"
@@ -182,10 +179,10 @@
                             <td>
                                 <translation-input
                                     :initial-translation="translation.translations[language]"
-                                    :language="language" 
+                                    :language="language"
                                     :group="translation.group"
                                     :translation-key="translation.key"/>
-                            </td>                            
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -266,7 +263,7 @@ export default {
         listTranslations() {
             this.$nextTick(async () => {
                 const translations = await axios.get(
-                    `/nova-vendor/nova-translation/languages/${this.language}/translations`, 
+                    `/nova-vendor/nova-translation/languages/${this.language}/translations`,
                     {
                         params: {
                             group: this.group,
@@ -276,7 +273,7 @@ export default {
                         }
                     }
                 );
-                
+
                 this.sourceLanguage = translations.data.source_language;
                 this.languages = translations.data.languages;
                 this.groups = translations.data.groups;
